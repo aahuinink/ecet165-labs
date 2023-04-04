ff40c449710b1980624434164d792c45d0200d09
aahuinink <a_huinink@hotmail.ca>
2023-04-04 10:06:43 -0700

    created new project for lab 12

---
+++ lab12/serial18f.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   serial18f.h
+ * Author: a_hui
+ *
+ * Created on April 4, 2023, 10:05 AM
+ */
+
+#ifndef SERIAL18F_H
+#define	SERIAL18F_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SERIAL18F_H */
+
